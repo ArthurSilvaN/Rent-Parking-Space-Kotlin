9deb2b1d7174a5a374a842c37dd9fd22238f4f2c
ArthurSilvaN <art.silvarn@gmail.com>
2021-03-13 16:12:25 -0300

    Splash screen created
    Empty screen as modified to splash screen

---
+++ app/src/main/java/com/example/rps_rentparkingspace/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.rps_rentparkingspace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+
+
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        changeToLogin()
+        }
+
+    private fun changeToLogin() {
+        val intent = Intent( this, LoginActivity::class.java);
+        Handler().postDelayed( {
+            intent.change()
+        },  2000)
+    }
+    fun Intent.change() {
+        startActivity(this)
+        finish()
+    }
+
+}
