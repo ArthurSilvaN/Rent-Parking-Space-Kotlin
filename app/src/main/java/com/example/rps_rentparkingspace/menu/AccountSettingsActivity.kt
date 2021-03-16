858f84cb7b64bfbb2fe839ba7895066c79520a0a
ArthurSilvaN <art.silvarn@gmail.com>
2021-03-16 12:14:41 -0300

    Layout Settings Account created

--- app/src/main/java/com/example/rps_rentparkingspace/menu/AccountSettingsActivity.kt
+++ app/src/main/java/com/example/rps_rentparkingspace/menu/AccountSettingsActivity.kt
@@ -1,8 +1,13 @@
 package com.example.rps_rentparkingspace.menu
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.example.rps_rentparkingspace.R
+
 
 class AccountSettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
